118980cf68fb0a30b717afe80ef0fa8eddb4d5e7
EarnestMD <85481264+EarnestMD@users.noreply.github.com>
2024-05-16 00:35:44 +0200

    Update MyProject.java

--- MyProject.java
+++ MyProject.java
@@ -1,6 +1,6 @@
 public class MyProject{
 	public static void main(String[] args){
 		System.out.println("Hello world");
-		System.out.println("Hello sample text");
+		System.out.println("Hello sample");
 	}
 }
